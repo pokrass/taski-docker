16c4070d00cf6e40c0cababbd6e34629e1893e47
Polina Petrova <p.petrova.a@yandex.ru>
2024-01-28 18:53:43 +0300

    Add flake8 config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,11 +63,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-	'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-	'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', 5432)
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
